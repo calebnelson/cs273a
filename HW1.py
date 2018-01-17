99347626205b0a27ed6f1be2a9eaba32ab484dad
calebnelson <calebnelson@gmail.com>
2018-01-17 15:27:36 -0800

    finished 1b?

--- HW1.py
+++ HW1.py
@@ -4,9 +4,10 @@ import matplotlib
 #Problem 1a
 alpha = 1
 ccenter1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-ccenter2 = [5, 9, 2, 8, 3, 7, 4, 6, 1]
+#ccenter2 = [5, 9, 2, 8, 3, 7, 4, 6, 1]
+ccenter2 = [3, 2.5, 1, 6, 4.5, 4, 9, 8.5, 7]
 
-N = 100
+N = 10000
 cov = np.identity(9) * alpha
 set1 = np.random.multivariate_normal(ccenter1, cov, N)
 set2 = np.random.multivariate_normal(ccenter2, cov, N)
@@ -31,7 +32,27 @@ sw2 = find_sw(set2, m2)
 sw = sw1 + sw2
 weights = np.dot(np.linalg.inv(sw), (m2-m1))
 
-for row in set1:
-	print (np.dot(row, weights))
-for row in set2:
-	print (np.dot(row, weights))
+print "weights"
+print (weights)
+
+set_all = np.concatenate((set1, set2))
+m = np.zeros(9)
+for i in range(0, 9):
+	m[i] = np.mean(set_all[:, i])
+bias = np.dot(m, weights)
+print "bias"
+print bias
+
+set_test1 = np.random.multivariate_normal(ccenter1, cov, N/4)
+set_test2 = np.random.multivariate_normal(ccenter2, cov, N/4)
+num_errors = 0.0
+print "results for test set"
+for row in set_test1:
+	x = (np.dot(row, weights))
+	if (x - bias > 0):
+		num_errors +=1
+for row in set_test2:
+	x = (np.dot(row, weights))
+	if (x - bias < 0):
+		num_errors += 1
+print "error rate:" + str(num_errors/(N/2))
