d957ccbeec7c408b82d2a33ec283bc1f0cee218a
calebnelson <calebnelson@gmail.com>
2018-01-18 22:52:26 -0800

    fixed the code and added a lot of new stuff

--- HW1.py
+++ HW1.py
@@ -1,25 +1,6 @@
 import numpy as np
 import matplotlib
 
-#Problem 1a
-alpha = 1
-ccenter1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-ccenter2 = [3, 2.5, 1, 6, 4.5, 4, 9, 8.5, 7]
-
-ccenter1 = [1, 0, 0, 0, 0, 0, 0, 1, 0]
-ccenter2 = [0, 0, 1, 0, 0, 1, 0, 0, 0]
-
-N = 10000
-cov = np.identity(9) * alpha
-set1 = np.random.multivariate_normal(ccenter1, cov, N)
-set2 = np.random.multivariate_normal(ccenter2, cov, N)
-
-#Problem 1b
-m1 = np.empty(9)
-m2 = np.empty(9)
-for i in range(0, 9):
-	m1[i] = np.mean(set1[:, i])
-	m2[i] = np.mean(set2[:, i])
 
 def find_sw(seti, mi):
 	swk = np.zeros((9,9))
@@ -29,90 +10,123 @@ def find_sw(seti, mi):
 		swk += swi
 	return swk
 
-sw1 = find_sw(set1, m1)
-sw2 = find_sw(set2, m2)
-sw = sw1 + sw2
-weights = np.dot(np.linalg.inv(sw), (m2-m1))
+def get_mean_of_sets(set1, set2):
+	set_all = np.concatenate((set1, set2))
+	m = np.zeros(9)
+	for i in range(0, 9):
+		m[i] = np.mean(set_all[:, i])
+	return m
 
-set_all = np.concatenate((set1, set2))
-m = np.zeros(9)
-for i in range(0, 9):
-	m[i] = np.mean(set_all[:, i])
-bias = np.dot(m, weights)
-print "bias"
-print bias
+def find_bias(set1, set2, weights, m = np.zeros(9)):
+	if (not m.any()):
+		m = get_mean_of_sets(set1, set2)
+	return np.dot(m, weights)
 
-print "weights before training"
-print weights
+def fisher(set1, set2):
+	m1 = np.empty(9)
+	m2 = np.empty(9)
+	for i in range(0, 9):
+		m1[i] = np.mean(set1[:, i])
+		m2[i] = np.mean(set2[:, i])
 
-#Problem 1c
-test_ratio = .5
-learning_rate = 0.00001
-set_test1 = np.random.multivariate_normal(ccenter1, cov, (int)(N*(test_ratio/2)))
-set_test2 = np.random.multivariate_normal(ccenter2, cov, (int)(N*(test_ratio/2)))
+	sw1 = find_sw(set1, m1)
+	sw2 = find_sw(set2, m2)
+	sw = sw1 + sw2
+	weights = np.dot(np.linalg.inv(sw), (m2-m1))
+	return weights
 
-ne1 = 0
-ne2 = 0
-print "results for test set before learning"
-for i in range((int)(N*test_ratio)):
-	if (i % 2 == 0):
-		row = set_test1[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias >= 0):
-			ne1 += 1
+def perceptron(set1, set2, learning_rate, p = True):
+	m = get_mean_of_sets(set1, set2)
+	N = len(set1) + len(set2)
+	weights = np.zeros(9)
+	bias = find_bias(set1, set2, weights, m)
+	for epoch in range(200):
+		tep = 0
+		for i in range(N):
+			if (i % 2 == 0):
+				row = set1[i/2]
+				x = (np.dot(row, weights))
+				if (x - bias >= 0):
+					for i in range(9):
+						weights[i] -= row[i] * learning_rate
+					bias = find_bias(set1, set2, weights, m)
+					tep += x
 
-	else:
-		row = set_test2[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias < 0):
-			ne2 += 1
+			else:
+				row = set2[i/2]
+				x = (np.dot(row, weights))
+				if (x - bias < 0):
+					for i in range(9):
+						weights[i] += row[i] * learning_rate
+					bias = find_bias(set1, set2, weights, m)
+					tep += x
+		if(p):
+			print "Total Energy Potential at Epoch " + str(epoch) + ": " + str(tep)
+		if (tep == 0):
+			break
+	return weights
 
-print "num errors after test1"
-print ne1
-print "num errors after test2"
-print ne2
-print "error rate:" + str((ne1+ne2)/(N*test_ratio))
+def test_weights(set1, set2, weights, p = True):
+	N = len(set1) + len(set2)
+	bias = find_bias(set1, set2, weights)
+	ne1 = 0
+	ne2 = 0
+	for i in range(N):
+		if (i % 2 == 0):
+			row = set1[i/2]
+			x = (np.dot(row, weights))
+			if (x - bias >= 0):
+				ne1 += 1
 
-# learning step
-for i in range((int)(N*test_ratio)):
-	if (i % 2 == 0):
-		row = set_test1[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias >= 0):
-			for i in range(9):
-				weights[i] -= row[i] * weights[i] * learning_rate
+		else:
+			row = set2[i/2]
+			x = (np.dot(row, weights))
+			if (x - bias < 0):
+				ne2 += 1
+	if(p):
+		print "num errors after test1"
+		print ne1
+		print "num errors after test2"
+		print ne2
+		print "error rate:" + str((ne1+ne2)/N) + "\n"
+	return (ne1+ne2)/N
 
-	else:
-		row = set_test2[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias < 0):
-			for i in range(9):
-				weights[i] += row[i] * weights[i] * learning_rate
+def test_centers(ccenter1, ccenter2, alpha = 0.1, test_ratio = 0.1, learning_rate = 0.1, N = 10000):
+	cov = np.identity(9) * alpha
+	set1 = np.random.multivariate_normal(ccenter1, cov, N)
+	set2 = np.random.multivariate_normal(ccenter2, cov, N)
 
-#bias = np.dot(m, weights)
-print "\nnew bias"
-print bias
+	#Find the weights using just the fisher discriminant and
+	fisher_weights = fisher(set1, set2) #1b
+	#The weights using the perceptron learning algorithm
+	perceptron_weights = perceptron(set1, set2, learning_rate) #1c
 
-print "new weights"
-print weights
+	#Intialize our test data
+	set_test1 = np.random.multivariate_normal(ccenter1, cov, (int)(N*(test_ratio/2)))
+	set_test2 = np.random.multivariate_normal(ccenter2, cov, (int)(N*(test_ratio/2)))
+	
+	#Test our weights
+	print "Testing fisher weights"
+	fisher_err = test_weights(set1, set2, fisher_weights)
+	print "Testing perceptron weights"
+	perceptron_err = test_weights(set1, set2, perceptron_weights)
+	return (fisher_err, perceptron_err)
 
-ne1 = 0
-ne2 = 0
-print "results for test set after learning"
-for i in range((int)(N*test_ratio)):
-	if (i % 2 == 0):
-		row = set_test1[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias >= 0):
-			ne1 +=1
+if __name__ == "__main__":
+	alpha = 0.1
+	test_ratio = .1
+	learning_rate = 0.1
+	N = 10000
 
-	else:
-		row = set_test2[i/2]
-		x = (np.dot(row, weights))
-		if (x - bias < 0):
-			ne2 += 1
-print "num errors after test1"
-print ne1
-print "num errors after test2"
-print ne2
-print "error rate:" + str((ne1+ne2)/(N*test_ratio))
+	#Problem 1 using Gaussian distributions
+	print "Testing Gaussian"
+	ccenter1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	ccenter2 = [3, 2.5, 1, 6, 4.5, 4, 9, 8.5, 7]
+	test_centers(ccenter1, ccenter2)
+
+	#Problem 2 using 3x3 binary images
+	print "Testing Images"
+	ccenter3 = [1, 0, 0, 0, 0, 0, 0, 1, 0]
+	ccenter4 = [0, 0, 1, 0, 0, 1, 0, 0, 0]
+	test_centers(ccenter3, ccenter4)
+
