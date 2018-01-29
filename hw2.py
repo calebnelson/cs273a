20f0be43eaae62fa7a808f2494ca55042a983ce7
Caleb Nelson <calebnelson@gmail.com>
2018-01-29 13:42:35 -0800

    added daniel and madina's changes

--- hw2.py
+++ hw2.py
@@ -15,7 +15,7 @@ K is the number of classes
 learning_rate is the step size of the gradient descend
 eps is the stopping criteria
 '''
-def build_model(x, t, v_x, v_t, N, M, K, learning_rate, eps):
+def build_model(x, t, v_x, v_t, N, M, K, learning_rate, eps, target_accuracy, target_iter):
     #declare and initialize the 2 weight matrices and bias vectors
     np.random.seed(0)
     W1 = np.random.randn(N,M)/np.sqrt(N)
@@ -31,24 +31,13 @@ def build_model(x, t, v_x, v_t, N, M, K, learning_rate, eps):
         iter += 1
         #start training, break if the target accuracy has be achieved
         for i in range(0, len(x)):
-            #print('x', x)
-            #print('w1', W1)
-            #print('w2', W2)
-            #print('t:', t)
-
             a1 = np.dot(x[i], W1)+b1
-            #print('a1', a1)
             z1 = np.tanh(a1)
-            #print('z', z1)
             a2 = np.dot(z1, W2)+b2
-            #print('a2', a2)
             exp_scores = np.exp(a2)
             z2 = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-            #print('z2', z2)
             delta_k = z2-t[i]
-            #print('delta_k', delta_k)
             delta_j = (1 - np.power(z1, 2)) * (np.dot(delta_k, W2.T))
-            #print('delta_j', delta_j)
 
             #calculate gradients
             X = np.asmatrix(x[i])
@@ -64,15 +53,26 @@ def build_model(x, t, v_x, v_t, N, M, K, learning_rate, eps):
             b1 -= learning_rate*db1
             b2 -= learning_rate*db2
 
-            #print('np.log(z2):', np.log(z2))
-            #print('np.log(1-z2):', np.log(1-z2))
-            #print('np.dot(t, np.log(z2)):', np.dot(t, np.log(z2[0])))
-            #print('np.dot((1-t), np.log(1-z2)):', np.dot((1-t), np.log(1-z2[0])))
-            cross_entropy = np.dot(t[i], np.log(z2[0])) + np.dot((1-t[i]), np.log(1-z2[0]))
+        for i in range(0, len(v_x)):
+            a1 = np.dot(v_x[i], W1)+b1
+            z1 = np.tanh(a1)
+            a2 = np.dot(z1, W2)+b2
+            exp_scores = np.exp(a2)
+            z2 = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+            cross_entropy = np.dot(v_t[i], np.log(z2[0])) + np.dot((1-v_t[i]), np.log(1-z2[0]))
             total_cross_entropy -= cross_entropy
 
-        print('iteration: ', iter, 'cross_entropy: ', total_cross_entropy)
+        error_rate = test_model(W1, W2, b1, b2, v_x, v_t)
+        
+        print('iteration: ', iter, 'cross_entropy: ', total_cross_entropy, ' error rate: ', error_rate)
         if(abs(total_cross_entropy - prev_total_cross_entropy) < eps):
+            break
+        elif error_rate < target_accuracy:
+            print('early exit: target_accuracy met')
+            break
+        elif iter > target_iter:
+            print('early exit: max_iter exceeded')
             break
 
     return (W1, W2, b1, b2)
@@ -88,16 +88,18 @@ def test_model(W1, W2, b1, b2, x, t):
     for i in range(len(t)):
 	if (not np.all(rounded_z2[i] == t[i])):
 		num_errors += 1
-    return (num_errors/len(t))*100
+    return (num_errors/len(t))
 
 if __name__ == "__main__":
     
-    learning_rate = 0.1
+    learning_rate = 0.05
+    epsilon = 0.01
+    target_accuracy = 0.01
+    target_iter = 500
 
     x_1a, t_1a, plot_classes_1a = get_inputs("hw2_train_1a.npz")
     v_x_1a, v_t_1a, v_plot_classes_1a = get_inputs("hw2_validation_1a.npz")
     t_x_1a, t_t_1a, t_plot_classes_1a = get_inputs("hw2_test_1a.npz")
 
-    #print t_1a
-    (W1, W2, b1, b2) = build_model(x_1a, t_1a, v_x_1a,  v_t_1a,  2,10,2, learning_rate, 0.01)
-    print("Error rate: " + str(test_model(W1, W2, b1, b2, t_x_1a, t_t_1a)) + "%")
+    (W1, W2, b1, b2) = build_model(x_1a, t_1a, v_x_1a,  v_t_1a,  2,10,2, learning_rate, epsilon, target_accuracy, target_iter)
+    print("Error rate: " + str(test_model(W1, W2, b1, b2, t_x_1a, t_t_1a)*100) + "%")
